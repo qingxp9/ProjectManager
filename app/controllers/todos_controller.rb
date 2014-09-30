18ea4143b2791674faaf9f6bb27a70e2fd38eb29
qingxp9@gmail.com <qingxp9@gmail.com>
2014-09-30 11:35:44 +0800

    Finish project

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,22 @@
+class TodosController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end

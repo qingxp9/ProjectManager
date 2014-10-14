350fd3126fa93bc603d405257596d05fc30980ce
qingxp9@gmail.com <qingxp9@gmail.com>
2014-10-14 23:46:10 +0800

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   get 'projects/:project_id/todos/:id/edit_status', to: 'todos#edit_status'
   patch 'projects/:project_id/todos/:id/change_limit_time', to: 'todos#change_limit_time'
+  get 'events', to: 'events#index'
   devise_for :users
   root 'welcome#index'
 
   resources :teams
   resources :projects do
     resources :todos do
-      resources :comments
+      resources :comments, only: [:create, :destroy]
     end
   end
-  resources :events
   resources :accesses, only: [:create, :new, :update, :destroy, :edit]
 
 end
